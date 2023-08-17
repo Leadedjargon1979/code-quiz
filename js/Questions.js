1155fe619ae218f085bb78c70f65aec4a0e10b6a
Leadedjargon1979 <108930236+Leadedjargon1979@users.noreply.github.com>
2023-08-18 08:09:38 +1000

    Add files via upload

---
+++ js/Questions.js
@@ -0,0 +1,70 @@
+let questions = [
+    {
+        numb:1,
+        question: "What is javascript?",
+        answer:"JavaScript (JS) is a cross-platform, object-oriented programming language",
+        options: [
+            "high-level, general-purpose programming language.",
+            "JavaScript (JS) is a cross-platform, object-oriented programming language",
+            "Docker is a set of platform as a service products that use OS-level virtualization",
+            "is a series of small single-board computers developed"
+        ]
+    },
+    {
+        numb:2,
+        question:"What is a sprite?",
+        answer:"two-dimensional bitmap image that is integrated into a larger scene",
+        options:[
+            "two-dimensional bitmap image that is integrated into a larger scene",
+            "three-dimensional image",
+            "background image",
+            "8a2-b2"
+        ]
+
+    },
+{
+ numb:3,
+question:"What is HTML",
+answer:"standard markup language for documents designed to be displayed in a web browser",
+options:[
+    "standard markup language for documents designed to be displayed in a web browser",
+    "Used to design the website",
+    "objectoriented programming",
+    "1/2m+1"
+]
+
+},
+
+{
+    numb:4,
+    question:"What is rasberrypi?",
+    answer:"is a series of small single-board computers developed",
+    options:[
+        "objectoriented programming",
+        "is a series of small single-board computers developed",
+        "background image",
+        "2 and 2"
+    ]
+},
+{
+numb:5,
+question:"what symbol is used instead of semi-colon in python",
+answer:":",
+options:[
+    ";",
+    "=",
+    "] ",
+    "+"
+]
+
+
+},
+
+
+
+
+
+
+
+
+];
